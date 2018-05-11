3a4a8326777c71ac1f97ad4d27c5bba5172a8f80
Travis Kane <kevinthomaskane@gmail.com>
2018-05-10 17:38:44 -0700

    slider is funtional on side bar

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+// app.use('/', express.static('images'));
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+app.get("/css", function(req, res) {
+  res.sendFile(path.join(__dirname, "./style.css"));
+});
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
+
+// db.sequelize.sync().then(function() {
+//     app.listen(PORT, function() {
+//       console.log("App listening on PORT " + PORT);
+//     });
+//   });
